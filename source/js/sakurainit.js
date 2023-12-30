aa7f81d606696e93be534ac9caf39a48e04c43c7
夏沫花火zzz <xmcn@1l1.icu>
2023-12-30 20:37:03 +0800

    fix bug

--- source/js/sakurainit.js
+++ source/js/sakurainit.js
@@ -1,26 +1,26 @@
-var sakura_load = false
+var sakura_load = false;
 
 function sakura() {
-    if (!sakura_load) {
-        var sakura = new Sakura('body', {
-            colors: [
-                {
-                    gradientColorStart: 'rgba(255, 183, 197, 0.9)',
-                    gradientColorEnd: 'rgba(255, 197, 208, 0.9)',
-                    gradientColorDegree: 120,
-                },
-                {
-                    gradientColorStart: 'rgba(255,189,189)',
-                    gradientColorEnd: 'rgba(227,170,181)',
-                    gradientColorDegree: 120,
-                },
-                {
-                    gradientColorStart: 'rgba(212,152,163)',
-                    gradientColorEnd: 'rgba(242,185,196)',
-                    gradientColorDegree: 120,
-                },
-            ],
-        });
-        sakura_load = true
-    }
+  if (!sakura_load) {
+    var sakura = new Sakura("body", {
+      colors: [
+        {
+          gradientColorStart: "rgba(255, 183, 197, 0.9)",
+          gradientColorEnd: "rgba(255, 197, 208, 0.9)",
+          gradientColorDegree: 120,
+        },
+        {
+          gradientColorStart: "rgba(255,189,189)",
+          gradientColorEnd: "rgba(227,170,181)",
+          gradientColorDegree: 120,
+        },
+        {
+          gradientColorStart: "rgba(212,152,163)",
+          gradientColorEnd: "rgba(242,185,196)",
+          gradientColorDegree: 120,
+        },
+      ],
+    });
+    sakura_load = true;
+  }
 }
